d196cb14c82c097a45d4a3760298f125d6241aa7
Waltsuuu <waltteriheino@gmail.com>
2024-11-08 15:55:18 +0200

    STEP8 DONE. Configured morgan to show data sent in HTTP POST requests

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+var morgan = require('morgan')
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
 const app = express();
 
 app.use(express.json());
-
-var morgan = require('morgan')
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 // Tiny output
 // :method :url :status :res[content-length] - :response-time ms
 

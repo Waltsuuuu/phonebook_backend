749bba3e8d088ccfad27ff49a5169126da11a982
Waltsuuu <waltteriheino@gmail.com>
2024-11-11 13:03:53 +0200

    STEP 3.12 DONE. If password is the only parameter given to the program, it will return all phonebook entries

--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+
 const password = process.argv[2];
 
 const url = `mongodb+srv://waltteriheino:${password}@phonebook.zelxs.mongodb.net/?retryWrites=true&w=majority&appName=Phonebook`;
@@ -15,6 +16,18 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model("Person", personSchema);
 
+
+if (process.argv.length < 4) {
+    
+    Person.find({}).then((persons) => {
+        console.log("phonebook:")
+        persons.forEach((person) => {
+            console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+    });
+} else {
+
 const person = new Person({
     name: process.argv[3],
     number: process.argv[4],
@@ -24,3 +37,5 @@ person.save().then(result => {
     console.log(`added ${person.name} number ${person.number} to phonebook`)
     mongoose.connection.close()
 })
+
+};

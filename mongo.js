5bf9bbf3d241c9b2ed148cb5e17196d2e8d9887b
Waltsuuu <waltteriheino@gmail.com>
2024-11-11 12:37:20 +0200

    Added MongoDB connection and schema for saing phonebook entries. New entry is displayed in console

---
+++ mongo.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://waltteriheino:${password}@phonebook.zelxs.mongodb.net/?retryWrites=true&w=majority&appName=Phonebook`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+person.save().then(result => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+})

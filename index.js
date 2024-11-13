6a4fa716f1681d4d2c53b24339459a1c4ac5108c
Waltsuuu <waltteriheino@gmail.com>
2024-11-13 13:21:47 +0200

    Adde route for adding person to database and tested with .rest

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ app.get('/api/persons', (request, response) => {
 })
 
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+
+
 
 /*
 let persons = [
